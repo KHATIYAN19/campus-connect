c8d335ce9afbd7251bf1edffa013c60f4a385629
khatiyan19 <lavikhatiyan43177@gmail.com>
2024-11-21 17:24:24 +0530

    blocked function

--- backend/Routes/blockedRoute.js
+++ backend/Routes/blockedRoute.js
@@ -2,7 +2,7 @@ const express=require("express");
 const route=express.Router();
 const {auth,isAdmin}=require("../Middlewares/userMiddleware");
 const {Adduser,getAllblockedUser,removeUser}=require("../Controllers/blockedController");
-route.post("/addUser",auth,isAdmin,Adduser);
+route.post("/addUser",Adduser);
 route.get("/allUser" ,auth,getAllblockedUser);
 route.delete("/deleteUser/:email",auth,isAdmin,removeUser);
 module.exports=route;
